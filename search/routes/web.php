fea3412665bddc3bfc50bd0e7532041db41f1254
ismailrz <ismailcse14@gmail.com>
2019-07-06 17:29:10 +0600

    Search data from database

---
+++ search/routes/web.php
@@ -0,0 +1,23 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/tr', function () {
+    return view('welcome');
+});
+
+Route::get('/','StudentController@index')->name('index');
+Route::post('/store','StudentController@store')->name('store');
+
+
+// for Search
+Route::post('/search','StudentController@search')->name('search');
